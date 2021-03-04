7439c97a52a36e0945fc6d0e00d3dc5c8faef3f2
MarBasso <mikebravo37.ita@gmail.com>
2021-03-04 09:22:29 +0100

    Embed Thingspeak charts

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $(".confirm-button").click(() => {
   let fieldValue = Math.ceil(Math.random() * 10);
-  console.log(fieldValue);
   fetch('https://api.thingspeak.com/update?api_key=FVRA63GCX5781VVP&field2=' + fieldValue);
 });
